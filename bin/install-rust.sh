82c451ed85179ca3397d6a4f8cee56457b26974e
Jake Bukuts <jbukuts@gmail.com>
2024-01-21 03:32:44 -0500

    Update install-rust.sh

--- bin/install-rust.sh
+++ bin/install-rust.sh
@@ -9,12 +9,13 @@ source "$HOME/.cargo/env"
 
 # install the nightly toolchain
 echo "Installing nightly toolchain..."
-rustup toolchain install nightly
+rustup toolchain install nightly-x86_64-unknown-linux-gnu
 
 # change to nightly
 echo "Setting override..."
-rustup override set nightly
+rustup override set nightly-x86_64-unknown-linux-gnu 
 
 # set nightly to default
 # rustup defaut nightly
+
 
