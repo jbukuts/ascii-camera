618327e1a099458e227f6826fd02b3eb609d0fb6
jbukuts <jbukuts@gmail.com>
2024-01-21 01:55:28 -0500

    fix: script fiddle

--- bin/install-rust.sh
+++ bin/install-rust.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "Installing rustup..."
 # Install Rustup (compiler)
+echo "Installing rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 # Adding binaries to path
-
 echo "HOME: ${HOME}"
-
 source "$HOME/.cargo/env"
-source "/root/.cargo/env"
+# source "/root/.cargo/env"
 
 # install the nightly toolchain
 echo "Installing nightly toolchain..."
 rustup toolchain install nightly
+
 # change to nightly
 rustup override set nightly
+
 # set nightly to default
 # rustup defaut nightly
 
-#sanity check
-cargo --help
+./node_modules/.bin/wasm-pack --help
