0d58233c388d9636d82937d4514547593218fb60
jbukuts <jbukuts@gmail.com>
2024-01-21 01:28:55 -0500

    add vercel conf

--- bin/install-rust.sh
+++ bin/install-rust.sh
@@ -3,3 +3,5 @@ echo "Installing Rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Adding binaries to path
 source "$HOME/.cargo/env"
+# change to nightly
+rustup override set nighly
